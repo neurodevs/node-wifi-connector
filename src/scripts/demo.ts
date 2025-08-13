d6ca5037b933c4267cbc3471dbaf593c25fb9f5a
ericthecurious <hello@ericthecurious.com>
2025-08-12 23:53:07 -0700

    patch: create demo script

---
+++ src/scripts/demo.ts
@@ -0,0 +1,17 @@
+import AutoWifiConnector from '../modules/AutoWifiConnector'
+
+async function main() {
+    const instance = await AutoWifiConnector.Create({
+        ssid: 'RoArm-M2',
+        password: process.env.ROARM_M2_WIFI_PASSWORD || '',
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+
+    await instance.disconnect()
+}
+
+main().catch((error) => {
+    console.error('Error connecting to WiFi:', error)
+    process.exit(1)
+})
