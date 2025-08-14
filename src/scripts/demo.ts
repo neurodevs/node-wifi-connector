789ceeed16a327eb96d76f7c2cc17f5b7a28f086
ericthecurious <hello@ericthecurious.com>
2025-08-13 22:03:12 -0700

    patch: change instance local name for readability

--- src/scripts/demo.ts
+++ src/scripts/demo.ts
@@ -1,14 +1,14 @@
 import AutoWifiConnector from '../modules/AutoWifiConnector'
 
 async function main() {
-    const instance = await AutoWifiConnector.Create({
+    const wifi = await AutoWifiConnector.Create({
         ssid: 'RoArm-M2',
         password: process.env.ROARM_M2_WIFI_PASSWORD || '',
     })
 
     await new Promise((resolve) => setTimeout(resolve, 5000))
 
-    await instance.disconnect()
+    await wifi.disconnect()
 }
 
 main().catch((error) => {
